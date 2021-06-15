393012073a9e8a7133eb339a0b8224a448351abf
pergr <pergr@LAPTOP-DCILI4A1>
2021-06-15 09:20:58 +0200

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // String input = Console.ReadLine();
+            string[] input = Console.ReadLine().Split(' ', StringSplitOptions.None);
+
+            int x = int.Parse(input[0]);
+            int y = int.Parse(input[1]);
+            int n = int.Parse(input[2]);
+
+            for (int i = 1; i < n + 1; i++)
+            {
+
+                Console.WriteLine(i);
+            }
+
+           
+            
+            Console.ReadLine();
+        }
+    }
+}
